5318f09e450b00b514bac97f81e43fecf91f8227
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-01-30 11:13:03 +0900

    1～4の数字を入力するフォームを追加

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace TrampGame
     {
         static void Main(string[] args)
         {
+            //1. 1～4の数字を入力するフォームを4つ用意する。
+            for(int i = 0; i < 4; i++)
+            {
+                Console.Write("{0}番目のカードを入力してください。 > ",i + 1);
+                Console.ReadLine();
+            }
+            Console.ReadLine() ;
         }
     }
 }
